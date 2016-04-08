fe2213ccbaefa16756dea9a8073893ed5efbdf89
Zhicheng's Ubuntu <beyond6987@gmail.com>
2016-04-08 11:08:28 -0500

    remove verbose

--- loadUCR.lua
+++ loadUCR.lua
@@ -1,7 +1,7 @@
 require 'csvigo'
 
 function loadUCR(file_name)
-    f_train = csvigo.load({path = file_name, mode='raw'})
+    f_train = csvigo.load({path = file_name, verbose = false, mode='raw'})
 
     train_set = {}
     -- load data set
@@ -10,10 +10,11 @@ function loadUCR(file_name)
         table.insert(train_set, torch.Tensor(string.split(v[1], ' +')))
     end
 
+    len_train = train_set[1]:size(1)
     train_targets = {}
     for i = 1, #train_set do
-        table.insert(train_targets, train_set[i][1])
-        train_set[i] = train_set[i]:sub(2, 25)
+        table.insert(train_targets, train_set[i][1] - 1) -- start from zero
+        train_set[i] = train_set[i]:sub(2, len_train)
     end
     return train_set, train_targets
 end
